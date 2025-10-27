ec9aac629bc0bc72a105fca3733e5dec6d47afad
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2025-10-26 20:17:04 -0400

    Creacion de Services e Interfaces

---
+++ TicTacToe/Services/IMovimientosApiService.cs
@@ -0,0 +1,9 @@
+﻿using TicTacToe.Dtos;
+
+namespace TicTacToe.Services;
+
+public interface IMovimientosApiService
+{
+    Task<Resource<List<MovimientoResponse>>> GetMovimientoAsync(int partidaId);
+    Task<Resource<MovimientoResponse>> PostMovimiento(int PartidaId, string Jugador, int PosicionFila, int PosicionColumna);
+}
---
+++ TicTacToe/Services/IPartidaApiService.cs
@@ -0,0 +1,10 @@
+﻿using TicTacToe.Dtos;
+
+namespace TicTacToe.Services;
+
+public interface IPartidaApiService
+{
+    Task<Resource<List<PartidaResponse>>> GetPartidasAsync();
+    Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId);
+    Task<Resource<PartidaResponse>> PostPartida(int jugador1, int jugador2);
+}
